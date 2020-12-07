3c16b50e879f0fdf0c57350439b2d13f7820fa1d
Joe Lambert <joseph.lambert@convoy.com>
2020-12-07 09:13:16 -0800

    part 1

--- 2020/1203.ts
+++ 2020/1203.ts
@@ -1,4 +1,41 @@
 import { readInput, transformIdentity } from "../utils";
 const input = readInput("2020", "3", transformIdentity);
 
-console.log(input);
+type Coord = [number, number];
+const terrain = ['.', '#'];
+
+const walkHorizontal = (map: string, startIndex: number, distance: number): number => {
+  
+  const terminus = map.length;
+  const travelled = startIndex + distance;
+
+  return travelled >= terminus ? travelled - terminus : travelled;
+}
+
+const walkVertical = (startIndex: number, distance: number, nextMap:string,): number | null => {
+  if (nextMap == null) {
+    return null;
+  }
+
+  return startIndex + distance;
+}
+
+// right 3, down 1
+
+const countTrees = (map: string[], currentPosition:Coord, treeCount = 0):number => {
+  const [x,y] = currentPosition;
+  const nextX = walkHorizontal(map[y], x, 3);
+  const nextY = walkVertical(y,1,map[y+1]);
+
+  if(nextY == null) {
+    console.log(treeCount);
+    return;
+  }
+
+  const terrainKey = map[nextY][nextX];
+
+  return countTrees(map, [nextX, nextY], treeCount + terrain.indexOf(terrainKey));
+}
+
+// Part One
+countTrees(input, [0,0]);
