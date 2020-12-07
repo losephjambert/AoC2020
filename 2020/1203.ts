5abb623727b53c652b45e8d2496110f167a240e3
Joe Lambert <joseph.lambert@convoy.com>
2020-12-07 09:39:40 -0800

    part two solution

--- 2020/1203.ts
+++ 2020/1203.ts
@@ -20,22 +20,42 @@ const walkVertical = (startIndex: number, distance: number, nextMap:string,): nu
   return startIndex + distance;
 }
 
-// right 3, down 1
-
-const countTrees = (map: string[], currentPosition:Coord, treeCount = 0):number => {
+const countTrees = (map: string[], currentPosition:Coord, direction: Coord, treeCount = 0):number => {
   const [x,y] = currentPosition;
-  const nextX = walkHorizontal(map[y], x, 3);
-  const nextY = walkVertical(y,1,map[y+1]);
+  const [over, down] = direction;
+  const nextX = walkHorizontal(map[y], x, over);
+  const nextY = walkVertical(y, down, map[y + down]);
 
   if(nextY == null) {
-    console.log(treeCount);
-    return;
+    return treeCount;
   }
 
   const terrainKey = map[nextY][nextX];
 
-  return countTrees(map, [nextX, nextY], treeCount + terrain.indexOf(terrainKey));
+  return countTrees(map, [nextX, nextY], direction, treeCount + terrain.indexOf(terrainKey));
 }
 
 // Part One
-countTrees(input, [0,0]);
+// countTrees(input, [0,0], [3,1]);
+
+// Part Two
+// Right 1, down 1.
+// Right 3, down 1.
+// Right 5, down 1.
+// Right 7, down 1.
+// Right 1, down 2.
+
+const directions: Coord[] = [
+  [1,1],
+  [3,1],
+  [5,1],
+  [7,1],
+  [1,2],
+];
+
+const partTwo = (map: string[], directions: Coord[]): number => {
+  return directions.reduce((prev, direction): number => (countTrees(map, [0,0], direction) * prev), 1);
+};
+
+const partTwoSolution = partTwo(input, directions);
+console.log(partTwoSolution);
